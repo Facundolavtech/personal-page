74cb63a76c78431a7701c74167419adf3c14dc80
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-21 04:50:01 -0300

    refactor: move tabs to helper file

--- src/components/HomeTabs/index.tsx
+++ src/components/HomeTabs/index.tsx
@@ -1,72 +1,54 @@
 import {
-  HStack,
+  Center,
   Icon,
   Stack,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
-  Tabs,
+  Tabs as ChakraTabs,
   Text,
 } from "@chakra-ui/react";
-import { AiFillAppstore } from "react-icons/ai";
-import { RiArticleFill } from "react-icons/ri";
-import { GrStackOverflow } from "react-icons/gr";
 import Articles from "../Articles";
 import Projects from "../Projects";
 import Technologies from "../Technologies";
+import Tabs from "./tabs";
 
 const HomeTabs = () => {
   return (
     <Stack className="wrapped" my={12} as="section" id="articles">
-      <Tabs isLazy defaultIndex={0} variant="enclosed-colored">
-        <TabList display="flex" gap={3} borderBottom="none">
-          <Tab
-            bg="white"
-            color="gray.800"
-            borderRadius={6}
-            _selected={{ color: "white", bg: "gray.800" }}
-          >
-            <HStack spacing={4}>
-              <Icon as={RiArticleFill} />
-              <Text>Articles</Text>
-            </HStack>
-          </Tab>
-          <Tab
-            bg="white"
-            color="gray.800"
-            borderRadius={6}
-            _selected={{ color: "white", bg: "gray.800" }}
-          >
-            <HStack width="full" height="full" spacing={4}>
-              <Icon as={AiFillAppstore} />
-              <Text>Projects</Text>
-            </HStack>
-          </Tab>
-          <Tab
-            bg="white"
-            color="gray.800"
-            borderRadius={6}
-            _selected={{ color: "white", bg: "gray.800" }}
-          >
-            <HStack spacing={4}>
-              <Icon as={GrStackOverflow} />
-              <Text>Tech Stack</Text>
-            </HStack>
-          </Tab>
+      <ChakraTabs defaultIndex={0} variant="enclosed-colored">
+        <TabList display="flex" flexWrap="wrap" gap={3}>
+          {Tabs.map((t) => {
+            return (
+              <Tab
+                key={t.title}
+                width={{ base: "full", md: "auto" }}
+                bg="white"
+                color="gray.800"
+                borderRadius={6}
+                _selected={{ color: "white", bg: "gray.800" }}
+              >
+                <Center gap={4}>
+                  <Icon as={t.icon} />
+                  <Text>{t.title}</Text>
+                </Center>
+              </Tab>
+            );
+          })}
         </TabList>
         <TabPanels>
-          <TabPanel padding={0}>
+          <TabPanel>
             <Articles />
           </TabPanel>
-          <TabPanel padding={0}>
+          <TabPanel>
             <Projects />
           </TabPanel>
-          <TabPanel padding={0}>
+          <TabPanel>
             <Technologies />
           </TabPanel>
         </TabPanels>
-      </Tabs>
+      </ChakraTabs>
     </Stack>
   );
 };
---
+++ src/components/HomeTabs/tabs.ts
@@ -0,0 +1,20 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { GrStackOverflow } from "react-icons/gr";
+import { RiArticleFill } from "react-icons/ri";
+
+const Tabs = [
+  {
+    title: "Articles",
+    icon: RiArticleFill,
+  },
+  {
+    title: "Projects",
+    icon: AiFillAppstore,
+  },
+  {
+    title: "Technologies",
+    icon: GrStackOverflow,
+  },
+];
+
+export default Tabs;
